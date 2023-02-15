aa1d5d2f3db7022c2e0546fa4470a6cdada0ad40
Deepkunal <105753019+Deepkunal@users.noreply.github.com>
2023-02-15 11:48:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
 //your code here
 
+var ol=document.getElementById(todoList);
+
+function adTodo()
+	{
+		if(todoValue.value){
+			var li=document.createElement('li');
+			li.textContent=todoValue.value;
+			ol.appendChild(li);
+			todoValue.value="";
+		}
+	}
+btn.addEventListner('click',addTodo);
