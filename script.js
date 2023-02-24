dcf4ad3da0230f49891c721c0ca535cf113ed1c3
Deepkunal <105753019+Deepkunal@users.noreply.github.com>
2023-02-24 09:38:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
-//your code here
-var todoValue= document.getElementById("newTodoInput");
-var btn= document.getElementById("addTodoBtn");
-var ol= document.getElementById("todoList");
+var todoValue = document.getElementById("newTodoInput");
+var btn = document.getElementById("addTodoBtn");
+var ol = document.getElementById("todoList");
 
-function addTodo()
-	{
-		if(todoValue.value){
-			
-			var li=document.createElement('li');
-			
-			li.textContent=todoValue.value;
-			ol.appendChild(li);
-			todoValue.value="";
-		}
-	}
-btn.addEventListener('click',addTodo);
+function addTodo() {
+  //alert(todoValue.value);
+  if (todoValue.value) {
+    // we need to create a li
+    var li = document.createElement("li");
+    li.textContent = todoValue.value;
+    ol.appendChild(li);
+    todoValue.value = "";
+  }
+}
+
+btn.addEventListener("click", addTodo);
